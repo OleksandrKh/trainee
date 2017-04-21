7692bfc3c62cc3d2bb44739771c2c0fdba1a0342
OleksandrKh <khrypun.oleksandr@yahoo.com>
2017-04-21 22:14:37 +0300

    Search input

--- hello_redux/src/components/App.js
+++ hello_redux/src/components/App.js
@@ -6,14 +6,24 @@ class App extends Component {
     this.props.onAddTrack(this.trackInput.value);
     this.trackInput.value = ''
   }
+	findTrack() {
+		console.log('Find track', this.searchInput.value);
+		this.props.onFindTrack(this.searchInput.value);
+	}
   render() {
     return (
       <div>
-        <input type="text" ref={event => { this.trackInput = event }} />
-        <button onClick={this.addTrack.bind(this)}>Add track</button>
+				<div>
+					<input type="text" ref={event => { this.trackInput = event }} />
+					<button onClick={this.addTrack.bind(this)}>Add track</button>
+				</div>
+				<div>
+					<input type="text" ref={event => { this.searchInput = event }} />
+					<button onClick={this.findTrack.bind(this)}>Find track</button>
+				</div>
         <ul>
           {this.props.traks.map((item, index) => {
-            return <li key={index}>{item}</li>
+            return <li key={index}>{item.name}</li>
           })}
         </ul>
       </div>
@@ -23,12 +33,19 @@ class App extends Component {
 
 export default connect(
   state => ({
-    traks: state.traks
+    traks: state.traks.filter(track => track.name.includes(state.filterTracks))
   }),
   dispatch => ({
-    onAddTrack: (trackName) => {
-      if (trackName === '') { return; }
-      dispatch({ type: 'ADD_TRACK', payload: trackName })
-    }
+    onAddTrack: (name) => {
+			const payload = {
+				id: Date.now().toString(),
+				name
+			}
+      if (name === '') { return; }
+      dispatch({ type: 'ADD_TRACK', payload })
+    },
+		onFindTrack: (name) => {
+			dispatch({ type: 'FIND_TRACK', payload: name })
+		}
   })
 )(App);
--- hello_redux/src/components/reducers/index.js
+++ hello_redux/src/components/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import traks from './traks';
 import playlists from './playlists';
+import filterTracks from './filterTracks';
 
 export default combineReducers({
 	traks,
-	playlists
+	playlists,
+	filterTracks
 })
